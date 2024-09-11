df2af296525f8fddc2f5696e376ba87670edd727
merco <merco@LAPTOP-HVHJ3F5H>
2024-09-11 20:28:16 +0200

    Add project files.

---
+++ VoodooEngine.cpp
@@ -0,0 +1,354 @@
+#include "pch.h"
+#include "VoodooEngine.h"
+
+void SetupWindowParams(WNDCLASSEX &WindowClass, WNDPROC InputCallbackFunction)
+{
+	LPCWSTR ClassName = L"Class";
+	HINSTANCE HInstance = nullptr;
+	WindowClass.cbSize = sizeof(WNDCLASSEX);
+	WindowClass.lpfnWndProc = InputCallbackFunction;
+	WindowClass.cbClsExtra = 0;
+	WindowClass.cbWndExtra = 0;
+	WindowClass.hInstance = HInstance;
+	WindowClass.lpszClassName = ClassName;
+	WindowClass.lpszMenuName = nullptr;
+	WindowClass.hbrBackground = nullptr;
+	WindowClass.hIcon = nullptr;
+	WindowClass.hIconSm = nullptr;
+	WindowClass.hCursor = nullptr;
+
+	RegisterClassEx(&WindowClass);
+}
+
+void CreateAppWindow(SWindowParams &WindowParams)
+{
+	WindowParams.HWind = CreateWindow(
+		WindowParams.WindowClass.lpszClassName,
+		WindowParams.WindowTitle,
+		WindowParams.Fullscreen,
+		0, 0,
+		WindowParams.ScreenResolutionWidth,
+		WindowParams.ScreenResolutionHeight,
+		nullptr,
+		nullptr,
+		WindowParams.WindowClass.hInstance,
+		nullptr);
+}
+
+void SetCustomAppIcon(LPCWSTR IconFileName, HWND &HWind)
+{
+	// Load custom app icon from file (ico file format)
+	HANDLE CustomAppIcon = LoadImage(
+		0, IconFileName,
+		IMAGE_ICON, 0, 0,
+		LR_DEFAULTSIZE | LR_LOADFROMFILE);
+
+	// Assign the custom icon as the official icon of the app 
+	// (if no icon is found, then the engine will use default desktop icon)
+	if (CustomAppIcon)
+	{
+		SendMessage(HWind, WM_SETICON, ICON_SMALL, (LPARAM)CustomAppIcon);
+		SendMessage(HWind, WM_SETICON, ICON_BIG, (LPARAM)CustomAppIcon);
+		
+		SendMessage(GetWindow(HWind, GW_OWNER), WM_SETICON, ICON_SMALL, (LPARAM)CustomAppIcon);
+		SendMessage(GetWindow(HWind, GW_OWNER), WM_SETICON, ICON_BIG, (LPARAM)CustomAppIcon);
+	}
+}
+
+void UpdateAppWindow()
+{
+	MSG MSGMessage;
+	MSGMessage.message = WM_NULL;
+	while (PeekMessage(&MSGMessage, nullptr, 0, 0, PM_REMOVE))
+		DispatchMessage(&MSGMessage);
+}
+
+bool InputPressed(std::map<int, bool> StoredInputs, int InputToCheck)
+{
+	int KeyToCheck = InputToCheck;
+	auto Iterator = StoredInputs.find(KeyToCheck);
+	if (Iterator != StoredInputs.end())
+		return Iterator->second;
+ 
+	return false;
+}
+
+void BroadcastInput(std::vector<InputCallback*> StoredCallbacks, int Input, bool Pressed)
+{
+	for (int i = 0; i < StoredCallbacks.size(); i++)
+	{
+		StoredCallbacks[i]->BroadcastInput(Input, Pressed);
+	}
+}
+
+float GetSecondsPerFrame(
+	LARGE_INTEGER* StartCounter, LARGE_INTEGER* EndCounter, LARGE_INTEGER* Frequency)
+{
+	return ((float)(EndCounter->QuadPart - StartCounter->QuadPart) / (float)Frequency->QuadPart);
+}
+
+float SetNewFPSLimit(float NewFPSLimit)
+{
+	return 1.0f / NewFPSLimit;
+}
+
+float UpdateFrameRate(VoodooEngine* Engine)
+{
+	QueryPerformanceCounter(&Engine->EndCounter);
+	float SecondsPerFrame = GetSecondsPerFrame(
+		&Engine->StartCounter, &Engine->EndCounter, &Engine->Frequency);
+
+	if (SecondsPerFrame < Engine->TargetSecondsPerFrame)
+	{
+		DWORD sleep_ms;
+		sleep_ms = (DWORD)(1000 * (Engine->TargetSecondsPerFrame - SecondsPerFrame));
+		Sleep(sleep_ms);
+		while (SecondsPerFrame < Engine->TargetSecondsPerFrame)
+		{
+			QueryPerformanceCounter(&Engine->EndCounter);
+			SecondsPerFrame = GetSecondsPerFrame(
+				&Engine->StartCounter, &Engine->EndCounter, &Engine->Frequency);
+		}
+	}
+
+	QueryPerformanceCounter(&Engine->EndCounter);
+	SecondsPerFrame = GetSecondsPerFrame(
+		&Engine->StartCounter, &Engine->EndCounter, &Engine->Frequency);
+
+	Engine->Counts.QuadPart = Engine->EndCounter.QuadPart - Engine->StartCounter.QuadPart;
+	Engine->FPS.QuadPart = Engine->Frequency.QuadPart / Engine->Counts.QuadPart;
+	Engine->StartCounter = Engine->EndCounter;
+
+	return SecondsPerFrame;
+}
+
+void Update(VoodooEngine* Engine, float DeltaTime)
+{
+	for (int i = 0; i < Engine->StoredUpdateComponents.size(); i++)
+	{
+		Engine->StoredUpdateComponents[i]->Update(DeltaTime);
+	}
+}
+
+ID2D1HwndRenderTarget* SetupRenderer(ID2D1HwndRenderTarget* RenderTarget, HWND HWind)
+{
+	ID2D1Factory* Factory = nullptr;
+	HRESULT Result = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &Factory);
+
+	RECT WinRect;
+	GetClientRect(HWind, &WinRect);
+
+	Result = Factory->CreateHwndRenderTarget(
+		D2D1::RenderTargetProperties(),
+		D2D1::HwndRenderTargetProperties(
+			HWind, D2D1::SizeU(WinRect.right, WinRect.bottom)),
+		&RenderTarget);
+
+	return RenderTarget;
+}
+
+ID2D1Bitmap* CreateNewBitmap(ID2D1HwndRenderTarget* RenderTarget, 
+	const wchar_t* FileName, bool Flip)
+{
+	ID2D1Bitmap* NewBitmap = nullptr;
+	HRESULT Result;
+
+	// Create Wic factory
+	IWICImagingFactory* WicFactory = nullptr;
+	Result = CoCreateInstance(
+		CLSID_WICImagingFactory,
+		nullptr,
+		CLSCTX_INPROC_SERVER,
+		IID_PPV_ARGS(&WicFactory));
+
+	// Create decoder
+	IWICBitmapDecoder* Decoder = nullptr;
+	Result = WicFactory->CreateDecoderFromFilename(
+		FileName,
+		nullptr,
+		GENERIC_READ,
+		WICDecodeMetadataCacheOnDemand,
+		&Decoder);
+
+	// Create decoder frame
+	IWICBitmapFrameDecode* DecoderFrame = nullptr;
+	Result = Decoder->GetFrame(0, &DecoderFrame);
+
+	// Create a converter from WIC bitmap to Direct2D bitmap
+	IWICFormatConverter* WicConverter = nullptr;
+	Result = WicFactory->CreateFormatConverter(&WicConverter);
+
+	// If flip is true, then flip the image
+	if (Flip)
+	{
+		// Setup flip rotator
+		IWICBitmapFlipRotator* ImageFlip = nullptr;
+		Result = WicFactory->CreateBitmapFlipRotator(&ImageFlip);
+		Result = ImageFlip->Initialize(DecoderFrame, WICBitmapTransformFlipHorizontal);
+
+		// Setup converter (for flipped image)
+		Result = WicConverter->Initialize(
+			ImageFlip,
+			GUID_WICPixelFormat32bppPBGRA,
+			WICBitmapDitherTypeNone,
+			nullptr,
+			0,
+			WICBitmapPaletteTypeCustom);
+
+		if (ImageFlip)
+			ImageFlip->Release();
+	}
+	else
+	{
+		// Setup the converter (for NON flipped image)
+		Result = WicConverter->Initialize(
+			DecoderFrame,
+			GUID_WICPixelFormat32bppPBGRA,
+			WICBitmapDitherTypeNone,
+			nullptr,
+			0,
+			WICBitmapPaletteTypeCustom);
+	}
+
+	RenderTarget->CreateBitmapFromWicBitmap(
+		WicConverter, nullptr, &NewBitmap);
+	
+	if (WicFactory)
+		WicFactory->Release();
+	
+	if (Decoder)
+		Decoder->Release();
+	
+	if (DecoderFrame)
+		DecoderFrame->Release();
+	
+	if (WicConverter)
+		WicConverter->Release();
+
+	return NewBitmap;
+}
+
+SBitmap SetupBitmapParams(ID2D1Bitmap* BitmapToSetup)
+{
+	// Create bitmap struct
+	SBitmap BitmapSetup = {};
+
+	// Set the bitmap source the same size as the loaded image (created bitmap)
+	BitmapSetup.BitmapSource.X = BitmapToSetup->GetSize().width;
+	BitmapSetup.BitmapSource.Y = BitmapToSetup->GetSize().height;
+
+	// Since computer graphics start from left to right
+	// "BitmapOffsetLeft" is not changed since default at is "0"
+	BitmapSetup.BitmapOffsetRight.X = BitmapSetup.BitmapSource.X;
+	BitmapSetup.BitmapOffsetRight.Y = BitmapSetup.BitmapSource.Y;
+
+	return BitmapSetup;
+}
+
+void RenderByLayer(ID2D1HwndRenderTarget* Renderer, 
+	std::vector<BitmapComponent*> StoredBitmaps, int RenderLayer)
+{
+	for (int i = 0; i < StoredBitmaps.size(); i++)
+	{
+		// Go to next if bitmap is not current render layer
+		if (StoredBitmaps[i]->BitmapParams.RenderLayer != RenderLayer)
+			continue;
+
+		// Go to next if bitmap is not valid
+		if (!StoredBitmaps[i]->Bitmap)
+			continue;
+
+		// Don't render bitmap if set to be hidden in game
+		if (StoredBitmaps[i]->BitmapParams.HiddenInGame)
+			continue;
+
+		D2D_RECT_F DestRect =
+			D2D1::RectF(
+			StoredBitmaps[i]->ComponentLocation.X,
+			StoredBitmaps[i]->ComponentLocation.Y,
+			StoredBitmaps[i]->ComponentLocation.X +
+			StoredBitmaps[i]->BitmapParams.BitmapOffsetRight.X,
+			StoredBitmaps[i]->ComponentLocation.Y +
+			StoredBitmaps[i]->BitmapParams.BitmapOffsetRight.Y);
+
+		D2D_RECT_F SourceRect =
+			D2D1::RectF(
+			StoredBitmaps[i]->BitmapParams.BitmapOffsetLeft.X,
+			StoredBitmaps[i]->BitmapParams.BitmapOffsetLeft.Y,
+			StoredBitmaps[i]->BitmapParams.BitmapSource.X,
+			StoredBitmaps[i]->BitmapParams.BitmapSource.Y);
+
+		Renderer->DrawBitmap(
+			StoredBitmaps[i]->Bitmap,
+			DestRect,
+			StoredBitmaps[i]->BitmapParams.Opacity,
+			D2D1_BITMAP_INTERPOLATION_MODE_NEAREST_NEIGHBOR,
+			SourceRect);
+	}
+}
+
+void Render(ID2D1HwndRenderTarget* Renderer,
+	std::vector<BitmapComponent*> BitmapsToRender, int MaxNumRenderLayers)
+{
+	for (int i = 0; i < MaxNumRenderLayers; i++)
+	{
+		RenderByLayer(Renderer, BitmapsToRender, i);
+	}
+}
+
+void RenderCollisionRectangles(ID2D1HwndRenderTarget* Renderer,
+	std::vector<CollisionComponent*> CollisionRectsToRender)
+{
+	for (int i = 0; i < CollisionRectsToRender.size(); i++)
+	{
+		// If collision is set to none, skip that index
+		if (CollisionRectsToRender[i]->NoCollision)
+			continue;
+
+		const D2D1_COLOR_F Color = 
+			{CollisionRectsToRender[i]->CollisionRectColor.R, 
+			CollisionRectsToRender[i]->CollisionRectColor.G,
+			CollisionRectsToRender[i]->CollisionRectColor.B, 255};
+		ID2D1SolidColorBrush* Brush;
+		Renderer->CreateSolidColorBrush(Color, &Brush);
+		Renderer->DrawRectangle(
+			D2D1::RectF(
+			CollisionRectsToRender[i]->ComponentLocation.X, 
+			CollisionRectsToRender[i]->ComponentLocation.Y,
+			CollisionRectsToRender[i]->ComponentLocation.X +
+			CollisionRectsToRender[i]->CollisionRect.X,
+			CollisionRectsToRender[i]->ComponentLocation.Y +
+			CollisionRectsToRender[i]->CollisionRect.Y),
+			Brush);
+
+		Brush->Release();
+	}
+}
+
+SVector GetObjectLocation(Object* ObjectToSearch)
+{
+	return ObjectToSearch->Location;
+}
+
+void SetObjectLocation(Object* ObjectToUpdate, SVector NewLocation)
+{
+	ObjectToUpdate->Location = NewLocation;
+}
+
+void SetComponentRelativeLocation(
+	Object* ComponentOwner, TransformComponent* ComponentToUpdate, SVector NewLocation)
+{
+	ComponentToUpdate->ComponentLocation = 
+		{ComponentOwner->Location.X + NewLocation.X,
+		ComponentOwner->Location.Y + NewLocation.Y};
+}
+
+SVector GetComponentRelativeLocation(
+	Object* ComponentOwner, TransformComponent* ComponentToSearch)
+{
+	SVector CurrentComponentLocation =
+		{ComponentOwner->Location.X + ComponentToSearch->ComponentLocation.X,
+		ComponentOwner->Location.Y + ComponentToSearch->ComponentLocation.Y};
+
+	return CurrentComponentLocation;
+}
---
+++ VoodooEngine.h
@@ -0,0 +1,204 @@
+#pragma once
+
+// STL
+#include <map>
+#include <string>
+#include <vector>
+
+// Win32 API
+#include <Windows.h>
+#include <wincodec.h>
+#pragma comment(lib, "windowscodecs.lib")
+
+// Direct2D API
+#include <d2d1.h>
+#include <d2d1_1.h>
+#pragma comment(lib, "d2d1.lib")
+
+#ifdef VOODOOENGINE_EXPORTS
+#define VOODOOENGINE_API __declspec(dllexport)
+#else
+#define VOODOOENGINE_API __declspec(dllimport)
+#endif
+
+// Window
+//-------------------------------------------
+// Window parameters information
+struct SWindowParams
+{
+	HWND HWind = nullptr;
+	WNDCLASSEX WindowClass;
+	LPCWSTR WindowTitle;
+	int ScreenResolutionWidth;
+	int ScreenResolutionHeight;
+	int Fullscreen;
+};
+// Setup window class for preparing for window creation
+extern "C" VOODOOENGINE_API void SetupWindowParams(WNDCLASSEX &WindowClass, 
+	WNDPROC InputCallbackFunction);
+// Create window and return window handle
+extern "C" VOODOOENGINE_API void CreateAppWindow(SWindowParams &WindowParams);
+// This will set a custom assigned icon of the app window title/task bar
+extern "C" VOODOOENGINE_API void SetCustomAppIcon(LPCWSTR IconFileName, HWND &HWind);
+// Updates any changes made to the app window (e.g. dragging the window)
+extern "C" VOODOOENGINE_API void UpdateAppWindow();
+//-------------------------------------------
+
+// Input
+//-------------------------------------------
+class InputCallback
+{
+public:
+	virtual void BroadcastInput(int Input, bool Pressed){};
+};
+// Check if input is being pressed/released, returns a bool and is false if no input is found
+extern "C" VOODOOENGINE_API bool InputPressed(std::map<int, bool> StoredInputs, int InputToCheck);
+extern "C" VOODOOENGINE_API void BroadcastInput(std::vector<InputCallback*> StoredCallbacks, 
+	int Input, bool Pressed);
+//-------------------------------------------
+
+// Utility
+//-------------------------------------------
+// 2D vector
+struct SVector
+{
+	float X = 0;
+	float Y = 0;
+};
+// Color (RGB)
+struct SColor
+{
+	int R = 0;
+	int G = 0;
+	int B = 0;
+};
+//-------------------------------------------
+
+// Base component that all movable components inherit
+//-------------------------------------------
+class TransformComponent
+{
+public:
+	SVector ComponentLocation = {0,0};
+};
+//-------------------------------------------
+
+// Bitmap
+//-------------------------------------------
+// Bitmap information
+struct SBitmap
+{
+	int RenderLayer = 0;
+	float Opacity = 1;
+	bool HiddenInGame = false;
+	SVector BitmapOffsetLeft = {0,0};
+	SVector BitmapOffsetRight = {0,0};
+	SVector BitmapSource = {0,0};
+};
+// Bitmap component class
+class BitmapComponent : public TransformComponent
+{
+public:
+	ID2D1Bitmap* Bitmap = nullptr;
+	SBitmap BitmapParams = {};
+};
+// Create bitmap image 
+extern "C" VOODOOENGINE_API ID2D1Bitmap* CreateNewBitmap(
+	ID2D1HwndRenderTarget* RenderTarget, const wchar_t* FileName, bool Flip = false);
+// Setup bitmap struct
+extern "C" VOODOOENGINE_API SBitmap SetupBitmapParams(ID2D1Bitmap* CreatedBitmap);
+//-------------------------------------------
+
+// Collision detection
+//-------------------------------------------
+// Collision component class
+class CollisionComponent : public TransformComponent
+{
+public:
+	bool NoCollision = false;
+	int CollisionTag = 0;
+	std::vector<int> CollisionTagsToIgnore;
+	SColor CollisionRectColor = {0,0,0};
+	SVector CollisionRect = {0,0};
+	SVector CollisionRectOffset = {0,0};
+
+	virtual void OnBeginOverlap(){};
+	virtual void OnEndOverlap(){};
+};
+//-------------------------------------------
+
+// Renderer
+//-------------------------------------------
+extern "C" VOODOOENGINE_API ID2D1HwndRenderTarget* SetupRenderer(
+	ID2D1HwndRenderTarget* RenderTarget, HWND WindowHandle);
+extern "C" VOODOOENGINE_API void RenderByLayer(
+	ID2D1HwndRenderTarget* Renderer, std::vector<BitmapComponent*> StoredBitmaps, int RenderLayer);
+extern "C" VOODOOENGINE_API void Render(
+	ID2D1HwndRenderTarget* Renderer, std::vector<BitmapComponent*> BitmapsToRender, 
+	int MaxNumRenderLayers);
+extern "C" VOODOOENGINE_API void RenderCollisionRectangles(
+	ID2D1HwndRenderTarget* Renderer, std::vector<CollisionComponent*> CollisionRectsToRender);
+//-------------------------------------------
+
+// Update component
+//-------------------------------------------
+class UpdateComponent
+{
+public:
+	virtual void Update(float DeltaTime) = 0;
+};
+//-------------------------------------------
+
+// Object
+//-------------------------------------------
+// Base object class inherited by all objects in the game
+//-------------------------------------------
+class Object
+{
+public:
+	SVector Location = {0,0};
+};
+extern "C" VOODOOENGINE_API SVector GetObjectLocation(Object* ObjectToSearch);
+extern "C" VOODOOENGINE_API void SetObjectLocation(Object* ObjectToUpdate, SVector NewLocation);
+extern "C" VOODOOENGINE_API void SetComponentRelativeLocation(
+	Object* ComponentOwner, TransformComponent* ComponentToUpdate, SVector NewLocation);
+extern "C" VOODOOENGINE_API SVector GetComponentRelativeLocation(
+	Object* ComponentOwner, TransformComponent* ComponentToSearch);
+//-------------------------------------------
+
+// Voodoo engine class
+//-------------------------------------------
+class VoodooEngine : public InputCallback
+{
+public:
+	bool EngineRunning = false;
+	SWindowParams Window;
+	ID2D1HwndRenderTarget* Renderer = nullptr;
+	LARGE_INTEGER StartCounter;
+	LARGE_INTEGER EndCounter;
+	LARGE_INTEGER Counts;
+	LARGE_INTEGER Frequency;
+	LARGE_INTEGER FPS;
+	float TargetSecondsPerFrame = 1.0f / 80.0f;
+	float DeltaTime = 0;
+	std::vector<Object*> StoredObjects;
+	std::vector<UpdateComponent*> StoredUpdateComponents;
+	std::vector<CollisionComponent*> StoredCollisionComponents;
+	std::vector<BitmapComponent*> StoredBitmaps;
+	std::map<int, bool> StoredInputs;
+	std::vector<InputCallback*> StoredInputCallbacks;
+};
+//-------------------------------------------
+
+// Update function that calls update to all connected update components
+//-------------------------------------------
+extern "C" VOODOOENGINE_API void Update(VoodooEngine* Engine, float DeltaTime);
+//-------------------------------------------
+
+// Frame rate related
+//-------------------------------------------
+extern "C" VOODOOENGINE_API float GetSecondsPerFrame(
+	LARGE_INTEGER* StartCounter, LARGE_INTEGER* EndCounter, LARGE_INTEGER* Frequency);
+extern "C" VOODOOENGINE_API float SetNewFPSLimit(float NewFPSLimit);
+extern "C" VOODOOENGINE_API float UpdateFrameRate(VoodooEngine* Engine);
+//-------------------------------------------
